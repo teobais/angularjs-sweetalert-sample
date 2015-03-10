1b36c6b80387f966add78215ceb6070be529fa0c
toubou91 <thodoris.bais@gmail.com>
2015-03-10 20:28:27 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function validationCtrl($scope) {
+   var validUsername = "Thodoris Bais";
+   var validEmail = "thodoris.bais@gmail.com";
+   
+   $scope.reset = function(){
+		$scope.username = validUsername;
+		$scope.email = validEmail;
+   }   
+   
+   $scope.checkData = function() {
+		if ($scope.username != validUsername || $scope.email != validEmail) {
+			swal("No", "The data you provided are wrong!", "error");
+		} else {
+			sweetAlert("Hello world!");
+		}
+	}
+}
